64da9a73595f0e4d16467d782fc769064f6392bc
Andrew Brogan <youdoof@gmail.com>
2018-06-05 17:20:05 -0500

    DOM Manipulation 6/5/18
    Beginning to work on DOM manipulation with JavaScript. Gotta start somewhere!

---
+++ JavaScript/DOM Manipulation/select-manipulate/manipulating-style.js
@@ -0,0 +1,21 @@
+// Styles
+var tag = document.querySelector("p");
+
+tag.style.color = "purple";
+
+// toggle on or off the css class
+/* Define in CSS 
+
+.class-name {
+	color: purple;
+	fontSize: 76px;
+}
+
+*/
+
+var p = document.querySelector("p");
+// p.classList.add("big");
+// p.classList.remove("big");
+// p.classList.toggle("big");
+
+p.textContent
